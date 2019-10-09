b407af9032cb83b032bc29ab875c1668264a1281
root <root@localhost.creatusello.com>
2019-10-09 15:17:26 +0530

    add config files

---
+++ common/config/main-local.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=parliament',
+            'username' => 'root',
+            'password' => 'rutusha@123',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+// send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+//'useFileTransport' => false,//to send mails to real email addresses else will get stored in your mail/runtime folder
+            //comment the following array to send mail using php's mail function
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'chiefsrsmail@gmail.com',
+                'password' => 'chiefsrs@123',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+    ],
+];
